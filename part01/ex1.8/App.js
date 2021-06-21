e8ab6eb9b58213cab26ba5ffdc552fb869465683
Abner77 <30878451+Abner77@users.noreply.github.com>
2021-06-21 10:43:47 +0200

    Add files via upload
    ejercicio 1.8

---
+++ part01/ex1.8/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+
+
+const Statistics = ({good,neutral, bad}) => {
+
+  const all = good + neutral + bad
+
+  return (
+    <>
+    <h2>statistics</h2>         
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {all}</p>
+    <p>average {(good - bad) / all} </p>
+    <p>positive {(good / all) * 100 + "%"}</p>
+    </>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ good, setGood ] = useState(0)  
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+  
+  let all = good + neutral + bad
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button
+        handleClick={increaseGood}
+        text='good'
+      />
+      <Button
+        handleClick={increaseNeutral}
+        text='neutral'
+      />     
+      <Button
+        handleClick={increaseBad}
+        text='bad'
+      />  
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
