09608ddc6df7556c2667f2300bc6bed1692ca107
Abner77 <30878451+Abner77@users.noreply.github.com>
2021-06-21 10:53:35 +0200

    Add files via upload
    ejercicio 1.10

---
+++ part01/ex1.10/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+
+
+
+
+const Statistic = ({value, text})  => {
+  return (
+    <p>{text} {value}</p>
+  )
+
+}
+
+const Statistics = ({good,neutral, bad}) => {
+
+  const all = good + neutral + bad
+  if (all > 0)
+  {
+    return (
+      <div>
+        <h2>statistics</h2>         
+        <Statistic text="good" value={good}/>
+        <Statistic text="neutral" value={neutral}/>      
+        <Statistic text="bad" value={bad}/>            
+        <Statistic text="all" value={all}/>                  
+        <Statistic text="average" value={(good - bad) / all}/>            
+        <Statistic text="positive" value={(good / all) * 100 + "%"}/>                              
+      </div>
+    )
+  }
+  return (
+    <>
+      <h2>statistics</h2>   
+      <p>No feedback was given yet</p>
+    </>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ good, setGood ] = useState(0)  
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+  
+  let all = good + neutral + bad
+   
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button
+        handleClick={increaseGood}
+        text='good'
+      />
+      <Button
+        handleClick={increaseNeutral}
+        text='neutral'
+      />     
+      <Button
+        handleClick={increaseBad}
+        text='bad'
+      />        
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
