5995b0b7ef7b5f07182eec7500c00593fa549902
Abner77 <30878451+Abner77@users.noreply.github.com>
2021-08-11 18:43:25 +0200

    Add files via upload
    ejercicio 9.10

---
+++ part9/ex9.10/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import diagnoseRouter from './routers/diagnoseRouter';
+// import { Diagnose } from './types';
+
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/diagnoses', diagnoseRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
